7eb657d754cf123eb574f08936b8e2680617c3af
darkside <raghuram.shankar98@gmail.com>
2023-04-16 17:34:57 +0100

    get OCV SOC

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from src.GPR import gpr
 
 if "__ipython__":
     sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(os.getcwd()))))
-    %matplotlib inline
-    %matplotlib widget
     %load_ext autoreload
     %autoreload 2
 
 if __name__ == "__main__":
-    gpr_obj = gpr(df_path="datasets/pmattia-dataset/2017-05-12_6C-50per_3_6C_CH36.csv")
+    gpr_obj = gpr(df_path="datasets/pmattia-dataset/2017-06-30_4_65C-44per_5C_CH22.csv")
+    # gpr_obj = gpr(df_path="datasets/pmattia-dataset/2017-05-12_6C-50per_3_6C_CH36.csv")
     # gpr_obj = gpr(df_path="datasets/moura-fast-charging-dataset/Test180.csv")
     gpr_obj.plot_df_resistance()
+    gpr_obj.plot_OCV_SOC()
 
     print("Done")
--- src/GPR.py
+++ src/GPR.py
@@ -10,21 +10,46 @@ class gpr(gpytorch.models.ExactGP):
     df_path: str
 
     def __post_init__(self):
-        # self.df = pd.read_csv(self.df_path, skiprows=19)
         self.df = pd.read_csv(self.df_path)
         self.df = self.df.iloc[:-2]
-        # self.df["Ageing_Time"] = pd.to_datetime(self.df["DateTime"])
         self.df["Ageing_Time"] = self.df["Data_Point"]
-        # super(GPR, self).__init__(
-        #     train_inputs=self.df["Ageing_Time"], train_targets=self.df[""]
-        # )
-        self.soc = np.linspace(0, 1, 100)
+
+        # get first cycle for OCV
+        first_cycle_df = self.df[self.df["Cycle_Index"] == 0]
+
+        # get chg OCV
+        df_chgOCV = first_cycle_df[first_cycle_df["Step_Index"] == 5][
+            ["Voltage", "Charge_Capacity"]
+        ]
+
+        # get dchg OCV
+        df_dchgOCV = first_cycle_df[first_cycle_df["Step_Index"] == 6][
+            ["Voltage", "Discharge_Capacity"]
+        ]
+
+        # get discharge capacity
+        self.capacity = (
+            df_dchgOCV["Discharge_Capacity"].iloc[-1]
+            - df_dchgOCV["Discharge_Capacity"].iloc[0]
+        )
+
+        # get OCV as average of dchg and chg OCV
+        OCV = (
+            (df_chgOCV["Voltage"].to_numpy())[0 : len(df_dchgOCV)]
+            + np.flip(df_dchgOCV["Voltage"].to_numpy())
+        ) / 2
+
+        # get SOC points from OCV
+        SOC = np.linspace(0, 1, len(OCV))
+
+        # store OCV SOC as a df
+        self.df_OCV_SOC = pd.DataFrame({"OCV": OCV, "SOC": SOC})
 
     def ocv_lookup(self, soc_lookup):
         return np.argmin(self.soc - soc_lookup)
 
-    # def cell_model(self, soc):
-    #     ocv_lookup =
-
     def plot_df_resistance(self):
         self.df.plot.line(x="Ageing_Time", y="Internal_Resistance")
+
+    def plot_OCV_SOC(self):
+        self.df_OCV_SOC.plot.line(x="SOC", y="OCV")
